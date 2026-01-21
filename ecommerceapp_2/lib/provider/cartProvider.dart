0d2d77e872f716cbce0d06697ce57b703689b9a6
TLifeOfCris <crissandoval150201@gmail.com>
2026-01-21 17:59:27 -0600

    Agregando provider de cart

---
+++ ecommerceapp_2/lib/provider/cartProvider.dart
@@ -0,0 +1,23 @@
+import 'package:ecommerceapp_2/models/cart_item.dart';
+import 'package:ecommerceapp_2/models/productModel.dart';
+import 'package:flutter/material.dart';
+
+class Cartprovider extends ChangeNotifier{
+  final List<CartItem> _items = [];
+
+  List<CartItem> get items => _items;
+
+  bool contains(Product product) => _items.any((item) => 
+  item.product.id == product.id);
+
+  void addToCart(Product product){
+    final index = _items.indexWhere((item) => item.product.id == product.id);
+
+    if (index == -1){
+      _items.add(CartItem(product: product));
+    } else {
+      _items[index].quantity++;
+    }
+    notifyListeners();
+  }
+}
