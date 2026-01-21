03fa3618c184a92597a64fa3074215e540b0de1b
TLifeOfCris <crissandoval150201@gmail.com>
2026-01-21 17:15:02 -0600

    Se terminó con la primera parte de provider al igual que una lista de wishlist

--- ecommerceapp_2/lib/provider/productsProvider.dart
+++ ecommerceapp_2/lib/provider/productsProvider.dart
@@ -7,6 +7,35 @@ class Productsprovider extends ChangeNotifier{
     id: '1', 
     title: 'Iphone Case', 
     description: 'Silicone case for iphone',
-    price: 29.99)
+    price: 29.99),
+
+    Product(id: '2',
+    title: 'Wireless Headphones',
+    price: 199.99,
+    description: 'Noise cancelling'),
   ];
+
+
+  //Todos los productos
+  List<Product> get products => _products;
+
+  //Wishlist (likeados)
+  List<Product> get wishlist => _products.where((p) => p.isLiked).toList();
+
+  //Like / Unlike 
+  void toggleLike(String productId){
+    final index = _products.indexWhere((p) => p.id == productId);
+    //devuelve -1 cuando no encuentra nada entonces cuando devuelva ese número significa STOP
+    if (index == -1) return;
+
+    _products[index].isLiked = !_products[index].isLiked;
+    notifyListeners();
+  }
+
+
+  //context.read<ProductsProvider>().toggleLike(product.id); para corazon
+
+
+  // Todos los productos
+  
 }
