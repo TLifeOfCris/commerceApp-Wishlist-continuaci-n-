052376b3e9ea84c71cc6730b1c935d1c4c13caa1
TLifeOfCris <crissandoval150201@gmail.com>
2026-01-23 12:09:20 -0600

    Se agregó un product grid

---
+++ ecommerceapp_2/lib/widgets/product_grid.dart
@@ -0,0 +1,26 @@
+import 'package:ecommerceapp_2/provider/productsProvider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final productsProvider = context.watch<Productsprovider>();
+    final products = productsProvider.products;
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.75,
+        crossAxisSpacing: 12,
+        mainAxisSpacing: 12), 
+        itemCount: products.length,
+        
+        itemBuilder: (context, index){
+          final product = products[index];
+
+          return Card();
+        });
+  }
+}
