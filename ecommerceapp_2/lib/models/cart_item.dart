23e5da66a0262fbee11e697b09cb5cf6ae0a6db7
TLifeOfCris <crissandoval150201@gmail.com>
2026-02-04 15:55:26 -0600

    Se agregó String productId para identificar el producto en firebase

--- ecommerceapp_2/lib/models/cart_item.dart
+++ ecommerceapp_2/lib/models/cart_item.dart
@@ -2,9 +2,11 @@ import 'package:ecommerceapp_2/models/productModel.dart';
 
 class CartItem {
   final Product product;
+  final String productId;
   int quantity;
 
   CartItem({
+    required this.productId,
     required this.product,
     this.quantity = 1
   });
