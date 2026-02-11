39623736c625a9f2320c6642d67b762e4acd1227
TLifeOfCris <crissandoval150201@gmail.com>
2026-02-10 19:52:39 -0600

    Se agregó CartScreen con un pedazo de codigo

--- ecommerceapp_2/lib/screens/cartScreen.dart
+++ ecommerceapp_2/lib/screens/cartScreen.dart
@@ -1,10 +1,21 @@
+import 'package:ecommerceapp_2/provider/cartProvider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final cart = context.watch<Cartprovider>();
+    final items = cart.items;  //values
+    return  CupertinoPageScaffold(
+      child: SafeArea(child: items.isEmpty ? const Center(
+        child: Text('Cart its empty'),
+      ) : Column(
+
+      )),
+    );
   }
 }
