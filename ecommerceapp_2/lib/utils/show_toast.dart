7d28d5157f8fbea1478a75f159d5c7f925880b5d
TLifeOfCris <crissandoval150201@gmail.com>
2026-03-19 13:43:21 -0600

    Se empezóa a trabajar en el iOSToast

--- ecommerceapp_2/lib/utils/show_toast.dart
+++ ecommerceapp_2/lib/utils/show_toast.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/cupertino.dart';
+
+// Es una notificación ligeral y temporal que aparece en la parte inferior o superior de la pantalla
+// Da retroalimentación al usuario y sin interrumpir actividad actual
+// Se descarta automaticamente después de unos segundos y es común en SwiftUI y UIkit 
+void showiOSToast(BuildContext context, String message){
+  final overlay = Overlay.of(context);
+  if (overlay == null) return; 
+  late OverlayEntry overlayEntry;
+}
