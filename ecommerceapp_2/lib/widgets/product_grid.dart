a92d58efcc262cd2fdcaefefac6a5eb084f1631a
TLifeOfCris <crissandoval150201@gmail.com>
2026-03-20 13:44:21 -0600

    se agregó ProductCard a ProductGrid y se ve más limpio

--- ecommerceapp_2/lib/widgets/product_grid.dart
+++ ecommerceapp_2/lib/widgets/product_grid.dart
@@ -4,6 +4,7 @@
 import 'package:ecommerceapp_2/provider/cartProvider.dart';
 import 'package:ecommerceapp_2/provider/productsProvider.dart';
 import 'package:ecommerceapp_2/utils/show_toast.dart';
+import 'package:ecommerceapp_2/widgets/product_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -28,50 +29,7 @@ class ProductGrid extends StatelessWidget {
           itemBuilder: (context, index){
             final product = products[index];
       
-            return Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16)
-              ),
-              child: Column(
-                children: [
-                  //AQUÍ DEBERÍA DE IR UNA IMAGEN
-                  Expanded(child: Container(
-                    color: Colors.grey[300]
-                  ),),
-                  //  AQUÍ VA EL TITULO DEL PRODUCTO
-                  Text(product.title),
-                  //  AQUÍ VA EL PRECIO DEL PRODUCTO
-                  Text('\$${product.price}'),
-                  //DESCRIPCIÓN DE PRODUCTO FALTA AGREGARLE QUE SOLO PUEDA MOSTRAR UNA PARTE DE DICHA APRA QUE NO CRASHE O SE VEA FEO 
-                  Text(product.description),
-                  
-
-                  //Agregar un row
-                  // BOTÓN DE LIKE 
-                  Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                    IconButton(onPressed: (){
-                    productsProvider.toggleLike(product.id);}, 
-                    icon: Icon(product.isLiked ? Icons.favorite : Icons.favorite_border)),
-                  // AÑADIR A CARRITO
-                    IconButton(onPressed: (){
-                      context.read<Cartprovider>().addToCart(product);
-
-
-                      showiOSToast(context, '${product.title}');
-                    
-
-
-
-
-                    //Agregar CARTPROVIDER
-                  }, icon: Icon(Icons.add))
-                    ],
-                  )
-
-                ],
-              ),
-            );
+            return ProductCard(product: product);
           }),
     );
   }
